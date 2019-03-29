125b9153e7449b5e01ebc0fac500791f24124283
tarfand79 <48951862+tarfand79@users.noreply.github.com>
2019-03-29 22:48:25 +0430

    Update JSON.lua

--- libs/JSON.lua
+++ libs/JSON.lua
@@ -1053,7 +1053,6 @@ return OBJDEF:new()
 --
 
 -- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- @joker2828
+-- @tabriztellll
+-- @tabriztellll
