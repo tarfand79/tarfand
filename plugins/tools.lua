ae3b6536ef6aca058fc5cca121ff24068f7fc1da
tarfand79 <48951862+tarfand79@users.noreply.github.com>
2019-03-30 01:01:09 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 123456789 -- put Your ID here! <===
+local SUDO = 839365940 -- put Your ID here! <===
 local function index_function(user_id)
   for k,v in pairs(_config.admins) do
     if user_id == v[1] then
@@ -629,7 +629,3 @@ run = run
 }
 
 -- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
